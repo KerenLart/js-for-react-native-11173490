b85be908ea8a004506dfc008b0db9ffcf7e403e2
KerenLart <kklartey@st.ug.edu.gh>
2024-05-18 00:58:24 +0000

    created the function for the second task

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,18 +1,37 @@
 
-function processArray(arr){
+function processArray(num){
     const result = [];
 
-    for (let key in arr){
-        if(arr[key]%2==0){
-            result.push(arr[key]**2);
+    for (let key in num){
+        if(num[key]%2==0){
+            result.push(num[key]**2);
         }
         else{
-            result.push(arr[key]**3);
+            result.push(num[key]**3);
         }
     }
     return result;
 }
 
-const number = [1,2,3,4,5,6,7,8]
+const number = [1,2,8,4]
 const result = processArray(number)
 console.log(result)
+
+function formatArrayStrings(num,str){
+    const formattedString = [];
+    for (let key in str){
+        if (num[key]%2==0){
+            formattedString.push(str[key].toUpperCase()) 
+        }
+        else {
+            formattedString.push(str[key].toLowerCase())
+        }
+    }
+    return formattedString;
+}
+
+const str = ["boy","class","FOR","poll"]
+const newNum =[2,3,4,5]
+const num = processArray(newNum)
+const formattedString = formatArrayStrings(num,str)
+console.log(formattedString)
